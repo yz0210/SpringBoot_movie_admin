c318a4dc680d136d3d06336f849074249f6a82bf
yang-sir <youngzhun@qq.com>
2023-03-27 17:43:26 +0800

    优化代码+新建基类

---
+++ src/main/java/com/example/SpringBoot_movie_admin/entity/movieCate/MovieAge.java
@@ -0,0 +1,15 @@
+package com.example.SpringBoot_movie_admin.entity.movieCate;
+
+import com.example.SpringBoot_movie_admin.entity.BaseEntity;
+
+/**
+ * @author: yang-sir
+ * @date: 2023/3/27 13:11
+ * @projectName: SpringBoot_movie_admin
+ * @version: 1.0
+ */
+
+
+public class MovieAge extends BaseEntity {
+
+}
---
+++ src/main/java/com/example/SpringBoot_movie_admin/entity/movieCate/MovieSort.java
@@ -0,0 +1,15 @@
+package com.example.SpringBoot_movie_admin.entity.movieCate;
+
+import com.example.SpringBoot_movie_admin.entity.BaseEntity;
+
+/**
+ * @author: yang-sir
+ * @date: 2023/3/27 13:49
+ * @projectName: SpringBoot_movie_admin
+ * @version: 1.0
+ */
+
+
+public class MovieSort extends BaseEntity {
+
+}
